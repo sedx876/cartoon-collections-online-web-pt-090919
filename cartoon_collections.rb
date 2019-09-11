73785d8f01f578ae036d2c400099afce9495e3d8
Sharon Watkins <swatkins876@gmail.com>
2019-09-11 17:13:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 #def roll_call_dwarves# code an argument here
   ["Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey", "Doc"].each_with_index { |word, index| puts "#{index} #{word}" }
-end
 
 #def summon_captain_planet# code an argument here
  ["earth", "wind", "fire", "water", "heart"].collect { |word| puts word.capitalize + "!" }
